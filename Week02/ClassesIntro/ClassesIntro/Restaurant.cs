7d7f3df37a2580390ffdaa00e0c86b9e938df1c3
essigdme <Essigdm@gmail.com>
2019-02-13 19:31:21 -0500

    Restaurant class

---
+++ Week02/ClassesIntro/ClassesIntro/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesIntro
+{
+    class Restaurant
+    {
+        public string Name;
+        public string Owner;
+        public string FoodType;
+        public int Expenses;
+        public int Money;
+
+        public void NetRevenue()
+        {
+            int TotalMoney = Money - Expenses;
+            Console.WriteLine(Convert.ToInt32(TotalMoney));
+        }
+    }
+}
