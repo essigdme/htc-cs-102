9e0c6a307c229b5fa2d90db1a23650c1151cb3a6
David Essig <Essigdm@gmail.com>
2019-03-06 19:26:33 -0500

    Inheritance classes with organisms

---
+++ Week05/InheritanceIntro/InheritanceIntro/Organism.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InheritanceIntro
+{
+    class Organism
+    {
+        public string Name;
+        public int age;
+
+        public void sayAge()
+        {
+            MessageBox.Show(Name + "is " + age + " years old.");
+        }
+    }
+}
